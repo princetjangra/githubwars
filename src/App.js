798aecbfc8a67bf3c53879bbbc12f190cb2ae0d2
Prince <princetjangra@gmail.com>
2020-06-17 12:36:25 +0530

    declutter state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React from "react";
 import axios from "axios";
 import "./App.scss";
 
+const Results = (props) => {
+  return (
+    <div className="war-result">
+      <div>
+        <span>Forks</span>
+        {props.playerData.name}
+        {console.log(props.playerData)}
+      </div>
+      <div>
+        <span>Repositories</span>
+        {props.playerData.public_repos}
+      </div>
+      <div>
+        <span>Total STARS</span>
+        {props.playerData.stars}
+      </div>
+    </div>
+  );
+};
+
 class PlayerCard extends React.Component {
   render() {
     const { results } = this.props;
@@ -23,44 +43,33 @@ class PlayerCard extends React.Component {
               // background: "url(" + this.props.playerData.avatar_url + ")",
             }
           ></div>
-          <h2>
-            {this.props.playerData.name ? this.props.playerData.name : "Player"}
-          </h2>
-          <div className="war-result">
-            <div>
-              <span>Forks</span>
-              {this.props.playerData.name}
-            </div>
-            <div>
-              <span>Repositories</span>
-              {this.props.playerData.public_repos}
-            </div>
-            <div>
-              <span>Total STARS</span>
-              {this.props.stars}
-            </div>
-          </div>
-          <label>
-            GITHUB USERNAME
-            <br />
-            <input
-              type="text"
-              name="username"
-              placeholder="search..."
-              value={this.props.value}
-              onChange={this.props.query}
-              autoComplete="off"
-            />
-            <ul>
-              {results.length
-                ? results.map((item) => (
-                    <li key={item.id} onClick={this.props.setPlayer}>
-                      {item.login}
-                    </li>
-                  ))
-                : ""}
-            </ul>
-          </label>
+          <h2>{this.props.war ? this.props.playerData.name : "Player"}</h2>
+          {/* {console.log(this.props.playerData)} */}
+          {this.props.war ? (
+            <Results playerData={this.props.playerData} />
+          ) : (
+            <label>
+              GITHUB USERNAME
+              <br />
+              <input
+                type="text"
+                name="username"
+                placeholder="search..."
+                value={this.props.value}
+                onChange={this.props.query}
+                autoComplete="off"
+              />
+              <ul>
+                {results.length
+                  ? results.map((item) => (
+                      <li key={item.id} onClick={this.props.setPlayer}>
+                        {item.login}
+                      </li>
+                    ))
+                  : ""}
+              </ul>
+            </label>
+          )}
         </div>
       </div>
     );
@@ -75,10 +84,19 @@ class App extends React.Component {
     player2: "",
     results1: {},
     results2: {},
-    player1res: {},
-    player2res: {},
-    stars1: 0,
-    stars2: 0,
+    player1res: {
+      name: "",
+      public_repos: 0,
+      stars: 0,
+    },
+    player2res: {
+      name: "",
+      public_repos: 0,
+      stars: 0,
+    },
+    // stars1: 0,
+    // stars2: 0,
+    war: false,
   };
 
   cancel = "";
@@ -103,7 +121,6 @@ class App extends React.Component {
 
   setPlayer2 = (ev) => {
     const player = ev.currentTarget.textContent;
-    console.log(player);
     this.setState({ player2: player, query2: player });
   };
 
@@ -111,7 +128,6 @@ class App extends React.Component {
     const searchUrl = `https://api.github.com/search/users?q=${query}&page=1&per_page=4`;
 
     if (this.cancel) this.cancel.cancel();
-
     this.cancel = axios.CancelToken.source();
 
     axios
@@ -138,18 +154,28 @@ class App extends React.Component {
   fetchUser = (username, num) => {
     const searchUrl = `https://api.github.com/users/${username}`;
 
-    if (this.cancel) this.cancel.cancel();
-    this.cancel = axios.CancelToken.source();
+    // if (this.cancel) this.cancel.cancel();
+    // this.cancel = axios.CancelToken.source();
 
     axios
-      .get(searchUrl, { cancelToken: this.cancel.token })
+      // .get(searchUrl, { cancelToken: this.cancel.token })
+      .get(searchUrl)
       .then((res) => {
         const userData = res.data;
+        // console.log(userData);
         num === 1
-          ? this.setState({ player1res: userData })
-          : this.setState({ player2res: userData });
-
-        this.setState({ player1res: res.data });
+          ? this.setState({
+              player1res: {
+                name: userData.name,
+                public_repos: userData.public_repos,
+              },
+            })
+          : this.setState({
+              player2res: {
+                name: userData.name,
+                public_repos: userData.public_repos,
+              },
+            });
       })
       .catch((err) => console.log(err));
   };
@@ -157,30 +183,42 @@ class App extends React.Component {
   fetchStarred = (username, num) => {
     const searchUrl = `https://api.github.com/users/${username}/starred`;
 
-    if (this.cancel) this.cancel.cancel();
-    this.cancel = axios.CancelToken.source();
+    // if (this.cancel) this.cancel.cancel();
+    // this.cancel = axios.CancelToken.source();
 
-    axios.get(searchUrl, { cancelToken: this.cancel.token }).then((res) => {
+    // axios.get(searchUrl, { cancelToken: this.cancel.token }).then((res) => {
+    axios.get(searchUrl).then((res) => {
       const length = res.data.length;
       num === 1
-        ? this.setState({ stars1: length })
-        : this.setState({ stars2: length });
+        ? this.setState({ player1res: { stars: length } })
+        : this.setState({ player2res: { stars: length } });
     });
   };
 
   start = () => {
+    this.state.war
+      ? this.setState({ war: false })
+      : this.setState({ war: true });
+
     const player1 = this.state.player1;
     const player2 = this.state.player2;
-    // if (player1) {
-    // const p1data = this.fetchUser(player1, 1);
-    // const p2data = this.fetchUser(player2, 2);
+
+    this.fetchUser(player1, 1);
+    this.fetchUser(player2, 2);
+
+    // console.log("fetched users");
+
     this.fetchStarred(player1, 1);
     this.fetchStarred(player2, 1);
-    // }
+
     // const p2data = this.fetchUser(player2);
     // console.log(p)
-    // this.setState({player1res: p1data})
-    // this.setState({player2res: p2data})
+    // this.setState({
+    //   player1res: { name: p1data.name, public_repos: p1data.public_repos },
+    // });
+    // this.setState({
+    //   player2res: { name: p2data.name, public_repos: p2data.public_repos },
+    // });
   };
 
   render() {
@@ -198,7 +236,8 @@ class App extends React.Component {
                 value={this.state.query1}
                 results={this.state.results1}
                 playerData={this.state.player1res}
-                stars={this.state.stars1}
+                // stars={this.state.stars1}
+                war={this.state.war}
               />
 
               <PlayerCard
@@ -207,11 +246,12 @@ class App extends React.Component {
                 value={this.state.query2}
                 results={this.state.results2}
                 playerData={this.state.player2res}
-                stars={this.state.stars2}
+                // stars={this.state.stars2}
+                war={this.state.war}
               />
             </div>
             <button className="startWar" onClick={this.start}>
-              START WAR
+              {this.state.war ? "START AGAIN" : "START WAR"}
             </button>
           </div>
 
